08f2298996506072d6bf4b50702a283a70a44884
Tommy Rogers <tommysayshello@gmail.com>
2012-07-22 20:07:00 -0700

    Use small images for 2D transform.

--- src/sr/test.clj
+++ src/sr/test.clj
@@ -6,15 +6,17 @@
 (def img1a "data/1d-1.png")
 (def img1b "data/1d-2.png")
 
-(def img2a "data/0.jpg")
-(def img2b "data/1.jpg")
+(def img2a "data/small0.jpg")
+(def img2b "data/small1.jpg")
 
 (def fnames [img1a img1b])
 
 (def twod [img2a img2b])
 
-
 (def features
+ '({:u [23 37], :x [8 35]} {:u [96 55], :x [86 57]} {:u [108 14], :x [97 17]} {:u [33 26], :x [21 26]} {:u [43 7], :x [32 6]} {:u [78 74], :x [65 75]} {:u [70 10], :x [87 8]} {:u [72 9], :x [84 7]}))
+
+(def features-large-size
  '({:u [449 176], :x [390 178]}
    {:u [138 98], :x [84 99]}
    {:u [85 218], :x [31 223]}
