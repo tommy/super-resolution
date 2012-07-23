1a8231fa91da27c2f05841753ef59999799a8e9e
Tommy Rogers <tommysayshello@gmail.com>
2012-07-22 20:06:08 -0700

    Add framework for tracking progress of long-running tasks.

--- src/sr/logging.clj
+++ src/sr/logging.clj
@@ -13,3 +13,72 @@
        (let [a# ~expr]
          (log ~level (str "Done with " '~expr))
          a#))))
+
+
+(defonce tasks (ref {}))
+
+(defn- tget
+  [id]
+  (get @tasks id))
+
+(defn task
+  "Create a new task. If a max value is specified, the task is expected
+  to be incremental."
+  ([id]
+  (dosync
+    (alter tasks assoc id (atom {:percent 0.0}))))
+  ([id max]
+  (dosync
+    (alter tasks assoc id (atom {:max max :now 0})))))
+
+(defn task-set
+  "Sets the current value of the task keyed by id.
+
+  If v is a float between 0 and 1, it is taken as the percentage completion.
+  Otherwise, v is number of completed steps towards the max. (i.e., v/max.)"
+  [id v]
+  (let [a (tget id)]
+    (if (float? v)
+      (reset! a {:percent v})
+      (swap! a assoc :now v))))
+
+(defn task-inc
+  "Increments the current number of completed steps towards the max."
+  [id]
+  (let [a (tget id)]
+    (swap! a update-in [:now] inc)))
+  
+
+(defn- percentage
+  [v]
+  (or
+    (:percent v)
+    (/ (:now v) (:max v))))
+
+(defn progress
+  "Returns the percentage completion of the progress indicator keyed by k."
+  [id]
+  (float (percentage @(tget id))))
+
+(defmacro task-item-macro
+  "Executes the body and then increments the item count in the task keyed
+  by id.
+
+  Example:
+
+  (task :something 50)
+  (doseq [i (range 50)]
+    (task-item :something
+      (some-operation i)))"
+
+  [id expr]
+  `(do
+     (let [a# ~expr]
+       (task-inc ~id)
+       a#)))
+
+(defn as-task-item
+  [id f]
+  (fn [& args]
+    (task-inc id)
+    (apply f args)))
