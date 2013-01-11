705a9e372d8319e2b888bd66bae91c259b76dcda
Tommy Rogers <tommysayshello@gmail.com>
2013-01-10 20:38:06 -0800

    Add drowzee images as test images.

--- src/sr/test.clj
+++ src/sr/test.clj
@@ -10,25 +10,13 @@
 (def img2b "data/small1.jpg")
 
 (def fnames [img1a img1b])
-
 (def twod [img2a img2b])
 
-(def features
- '({:u [23 37], :x [8 35]} {:u [96 55], :x [86 57]} {:u [108 14], :x [97 17]} {:u [33 26], :x [21 26]} {:u [43 7], :x [32 6]} {:u [78 74], :x [65 75]} {:u [70 10], :x [87 8]} {:u [72 9], :x [84 7]}))
+(def drowzees ["data/drowzee.png" "data/drowzeeB.png"])
 
-(def features-large-size
- '({:u [449 176], :x [390 178]}
-   {:u [138 98], :x [84 99]}
-   {:u [85 218], :x [31 223]}
-   {:u [152 261], :x [99 265]}
-   {:u [323 292], :x [271 295]}
-   {:u [394 176], :x [342 176]}
-   {:u [282 33], :x [233 36]}
-   {:u [209 66], :x [159 66]}))
-
+(comment
 (def pairs
   (map (juxt :u :x) features))
-
 (def uxs
   (map (comp first :u) features))
 (def uys
@@ -37,6 +25,7 @@
   (map (comp first :x) features))
 (def xys
   (map (comp second :x) features))
+)
 
 (defn -main [& args]
-  (open twod 2))
+  (open drowzees 2))
