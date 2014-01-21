40d508d6146b90e04303b55107c490bcd64e851c
Tommy Rogers <tommysayshello@gmail.com>
2014-01-20 18:16:24 -0800

    Add a 2d example with three photos.

--- src/sr/test.clj
+++ src/sr/test.clj
@@ -3,6 +3,7 @@
 
 (def oned ["data/1d-1.png" "data/1d-2.png"])
 (def twod ["data/small0.jpg" "data/small1.jpg"])
+(def hike ["data/hikeU.jpg" "data/hikeB.jpg" "data/hikeC.jpg"])
 
 (def drowzees ["data/drowzee.png" "data/drowzeeB.png"])
 
